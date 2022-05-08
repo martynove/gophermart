d8c5322228319681c978f954e2fceaee35d19901
Мартынов Евгений Владимирович <evgenii.martynov@domru.ru>
2022-05-08 15:25:21 +0500

    added migration from sql file in code

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,12 +1,23 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
 
 const (
 	usersTable = "users"
 )
 
 func NewPostgresDB(source string) (*sqlx.DB, error) {
+	log.Println(source)
+	if err := migrateDB(source); err != nil {
+		return nil, err
+	}
 	db, err := sqlx.Open("postgres", source)
 	if err != nil {
 		return nil, err
@@ -15,4 +26,15 @@ func NewPostgresDB(source string) (*sqlx.DB, error) {
 		return nil, err
 	}
 	return db, err
+}
+
+func migrateDB(source string) error {
+	m, err := migrate.New("file://migrations/schema", source)
+	if err != nil {
+		return fmt.Errorf("cannot init DB migrations: %w", err)
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("cannot apply migrations: %w", err)
+	}
+	return nil
 }
